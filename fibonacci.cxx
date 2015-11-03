218ec97c643e6dd6344f7ae4366dcfa7490a3e4b
Volodymyr Zholobenko <vzholobenko@pc07.pool.tp1.uni-duesseldorf.de>
2015-11-03 11:59:37 +0100

    done

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+  
+  int N;
+  cout << "N" << endl;
+  cin >> N;
+  int x0 = 0;
+  int x1 = 1;
+  int x;
+  
+  for(int i = 2; i<=N; i++) {
+    x = x1 + x0;
+    x0 = x1;
+    x1 = x;
+  }
+  
+  cout <<x<< endl;
+  
+  return 0;
+}
